28ba58ab0ce088e29df9816ae95addf0a6aa33cd
Wolfus20 <shawnpandev@gmail.com>
2024-06-15 14:59:40 +0800

    Implement recieve side of match scheduling

--- src/database/models.rs
+++ src/database/models.rs
@@ -1,5 +1,6 @@
 use std::fmt::Display;
 
+use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 /// The manager role configuration for a guild within the database.
@@ -100,11 +101,14 @@ pub enum PlayerType {
 }
 
 /// A match schedule within the database.
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize)]
 pub struct MatchSchedule {
-    match_id: uuid::Uuid,
-    proposed_time: i32,
-    time_of_proposal: chrono::DateTime<chrono::Utc>,
-    proposer: Option<i32>,
-    accepted: bool,
+    pub match_id: String,
+    pub proposed_time: DateTime<Utc>,
+    pub time_of_proposal: DateTime<Utc>,
+    /// Determines if player 1 or player 2 proposed the current schedule.
+    ///
+    /// Contains only 1 or 2.
+    pub proposer: i16,
+    pub accepted: bool,
 }
