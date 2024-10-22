e769b328a97d7f046481ded1bc301f6ccc6fe704
Darkness <kizluxury0506@gmail.com>
2024-10-22 22:21:52 +0700

    Co-authored-by: Shawn Pan <TofuWaffles@users.noreply.github.com>

--- dbcbot/migrations/20240330072940_create_tournaments.sql
+++ dbcbot/migrations/20240330072940_create_tournaments.sql
@@ -3,7 +3,7 @@ CREATE TYPE tournament_status AS ENUM ('pending', 'started', 'paused', 'inactive
 CREATE TYPE mode AS ENUM (
     'brawl_ball', 'gem_grab', 'heist', 'bounty', 'siege', 'solo_showdown', 
     'duo_showdown', 'hot_zone', 'knockout', 'takedown', 'lone_star', 'big_game', 
-    'robo_rumble', 'boss_fight', 'wipe_out', 'duels', 'paint_brawl', 
+    'robo_rumble', 'boss_fight', 'wipeout', 'duels', 'paint_brawl', 
     'brawl_ball5v5', 'gem_grab5v5', 'bounty5v5', 'knockout5v5', 'unknown'
 );
 CREATE TABLE IF NOT EXISTS tournaments (
