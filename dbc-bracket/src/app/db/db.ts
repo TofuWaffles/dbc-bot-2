cbc59e557d8e5865e8c0406e75633ed805a3db55
NightMat01 <nightmat01@gmail.com>
2024-10-28 19:58:57 +0000

    git messed with me again

--- dbc-bracket/src/app/db/db.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from 'pg';
-import * as dotenv from "dotenv";
-dotenv.config({ path: __dirname+'../../../../../../../.env' });
-
-export const pool: Pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-});
