98a97a84688c19343eea6b6ee64aa1f007677e46
NightMat01 <nightmat01@gmail.com>
2024-11-04 21:31:52 +0000

    fixed missing next base url routing

--- dbc-bracket/db/db.ts
+++ dbc-bracket/db/db.ts
@@ -6,20 +6,24 @@ dotenv.config({ path: `${process.cwd()}/../.env` });
 export class EnvVars {
     private static instance: EnvVars | null = null;
 
-    private constructor(private DATABASE_URL: string, private BRAWL_STARS_TOKEN?: string) {}
+    private constructor(private DATABASE_URL: string, private NEXT_PUBLIC_BASE_URL: string, private BRAWL_STARS_TOKEN?: string) {}
 
     public static fromEnv(): Result<EnvVars> {
-        if (!process.env.DATABASE_URL) {
+        if (!process.env.DATABASE_URL || !process.env.NEXT_PUBLIC_BASE_URL) {
             return Err(new Error("Required environment variables are missing."));
         }
         if (!this.instance) {
-            this.instance = new EnvVars(process.env.DATABASE_URL, process.env.BRAWL_STARS_TOKEN);
+            this.instance = new EnvVars(process.env.DATABASE_URL, process.env.NEXT_PUBLIC_BASE_URL, process.env.BRAWL_STARS_TOKEN);
         }
         return Ok(this.instance);
     }
 
     public getDatabaseUrl(): string {
         return this.DATABASE_URL;
+    }
+
+    public getNextPublicBaseUrl(): string {
+        return this.NEXT_PUBLIC_BASE_URL;
     }
 
     public getBrawlStarsToken(): string | undefined {
@@ -33,5 +37,7 @@ const envVars = expect(EnvVars.fromEnv());
 export const pool: Pool = new Pool({
     connectionString: envVars.getDatabaseUrl()
 });
+
+export const baseUrl: string = envVars.getNextPublicBaseUrl();
 
 export const BSToken: string = envVars.getBrawlStarsToken();
--- dbc-bracket/pages/services/tournament.ts
+++ dbc-bracket/pages/services/tournament.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 import "@/utils"
 import { Result, Err, Ok } from "@/utils";
 import { Tournament } from "@/db/models";
+import { baseUrl } from "@/db/db";
 const getAllTournamentsInAGuild = async (guildId: string): Promise<Result<Tournament[]>> => {
   const [response, error] = await axios.get<Tournament[]>(`/api/${guildId}`).wrapper();
   if(error){
@@ -12,8 +13,8 @@ const getAllTournamentsInAGuild = async (guildId: string): Promise<Result<Tourna
 
 const getAllTournaments = async (): Promise<Result<Tournament[]>> => {
   console.log("Fetching tournaments here");
-  const [response, error] = await axios.get<Tournament[]>("/api/").wrapper();
-  if(error){
+  const [response, error] = await axios.get<Tournament[]>(`${baseUrl}/api/`).wrapper();
+  if (error) {
     return Err(error);
   }
   return Ok(response.data);
