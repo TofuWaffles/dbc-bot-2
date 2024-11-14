0bc4eb55071e0a7a0c369766263b4c5ea53876ad
Darkness <kizluxury0506@gmail.com>
2024-11-15 03:05:55 +0700

    dockerize bracket

--- dbc-bracket/next.config.mjs
+++ dbc-bracket/next.config.mjs
@@ -3,6 +3,7 @@
 import webpack from 'webpack';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "standalone",
     webpack(config) {
         config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^pg-native$/ }));
         
