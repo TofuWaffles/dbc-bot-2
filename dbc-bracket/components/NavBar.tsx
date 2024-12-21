219f3ce8a33c59cf35b35ccc68129edad95bd8e0
Darkie <101615478+Darkiee12@users.noreply.github.com>
2024-12-21 19:18:22 +0700

    Update NavBar.tsx

--- dbc-bracket/components/NavBar.tsx
+++ dbc-bracket/components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Navbar = () => {
     return (
-        <nav className="bg-black w-full py-5">
+        <nav className="bg-black w-full py-5 flex justify-center items-center">
             <Link href="/"><p className="w-full text-center text-white font-bold text-2xl">Discord Brawl Cup</p></Link>
         </nav>
     );
